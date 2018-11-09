b76ec946b3a22998876ec8e4170bd695a7c96630
jr2339 <jr2339@nau.edu>
2018-11-08 22:43:37 -0700

    fix

--- lab.php
+++ lab.php
@@ -1,6 +1,6 @@
 //JavaScript-C24.2.0 (SpiderMonkey)
 
-var POP_SIZE = 20; //we generate 20 solutions
+var POP_SIZE = 100; //we generate 20 solutions
 var GENERATIONS = 50;
 
 function weighted_choice(items){
@@ -52,7 +52,6 @@ function fitness(solution){
             fitness = fitness + Math.abs(solution[i][0]-solution[i][1]);
         }
     }
-    
     return fitness;
 }   
 
@@ -66,7 +65,7 @@ function mutate(solution){
         if(determination == 1){
             var m = Math.floor(Math.random() * (solution.length-1));
             var n = Math.floor(Math.random() * (solution.length-1));
-            while( m==n){
+            while(m==n){
                     m = Math.floor(Math.random() * (solution.length-1));
                     n = Math.floor(Math.random() * (solution.length-1));
            }
@@ -81,34 +80,14 @@ function mutate(solution){
 }
 
 
-
-function valid(solution1,solution2,pos){
-    /*
-    var s1 =[];
-    var s2 =[];
-    for(var i=0; i<pos;i++){
-        s1.push(solution1[i][0]);
-        s1.push(solution1[i][1]);
-    }
-    for(var i=0;i<solution2.length-pos;i++){
-        s2.push(solution2[i][0]);
-        s2.push(solution2[i][1]);
-    }
-    
-    var found = false;
-    for(var i=0;i<2*pos;i++){
-        if(s2.indexOf(s1[i])>-1){
-            found = true;
-            break;
-        }
-    }
-    */
+function valid(solution1,solution2){
     var L1=[];
     var R1=[];
     var L2=[];
     var R2=[];
-    for(var i=0; i<solution1.length;i++){
-        if(i <pos){
+    var pos = solution1.length/2;
+    for(var i=0; i<2*pos;i++){
+        if(i < pos){
             L1.push(solution1[i][0]);
             L1.push(solution1[i][1]);
             L2.push(solution2[i][0]);
@@ -121,39 +100,46 @@ function valid(solution1,solution2,pos){
             R2.push(solution2[i][1]);            
         }
     }
+
     var found = false;
-    for(var i=0;i<2*pos;i++){
-        if(s2.indexOf(s1[i])>-1){
-            found = true;
-            break;
+    for(var i=0; i< R2.length; i++){
+        if(R1.indexOf(R2[i])>-1){
+                found = true;
+                break;
         }
     }
     
+   for(var i=0; i< L1.length; i++){
+        if(L2.indexOf(L1[i])>-1){
+            found = true;
+            break;
+        }
+   }
     return found;
 }
 
-//print(valid([[1,2],[3,4],[5,6],[7,8],[9,10],[11,12],[13,14],[15,16],[17,18],[19,20]],[[11,12],[13,14],[15,16],[17,18],[19,20],[1,2],[3,4],[5,6],[7,8],[9,10]],3))
+//print(valid([[12,10],[13,11],[8,20],[6,5],[14,3],[4,7],[17,18],[1,16],[19,15],[2,9]],[[12,10],[13,11],[8,20],[6,5],[14,3],[4,7],[17,18],[1,16],[19,15],[2,9]]))
 
-function crossover(solution1,solution2, pos){
+function crossover(solution1,solution2){
     var sch1=[];
     var sch2=[];
-    sch1 = (solution2.slice(solution2.length-pos)).concat(solution1.slice(pos));
-    sch2 = (solution2.slice(0,solution2.length-pos)).concat(solution1.slice(0,pos));
-    print(sch1);
-    print(sch2);
+    var pos = solution1.length/2;
+    sch1 = (solution2.slice(pos)).concat(solution1.slice(pos));
+    sch2 = (solution2.slice(0,pos)).concat(solution1.slice(0,pos));
     return [sch1,sch2];
 }
 
-crossover([[1,2],[3,4],[5,6],[7,8],[9,10],[11,12],[13,14],[15,16],[17,18],[19,20]],[[11,12],[13,14],[15,16],[17,18],[19,20],[1,2],[3,4],[5,6],[7,8],[9,10]],3);
+//crossover([[12,10],[13,11],[8,20],[6,5],[14,3],[4,7],[17,18],[1,16],[19,15],[2,9]],[[12,10],[13,11],[8,20],[6,5],[14,3],[4,7],[17,18],[1,16],[19,15],[2,9]]);
 
 function GA(SE){
-
     var population = random_population(SE);
+    var best_solution =[];
     for(var generation=0;generation<GENERATIONS;generation++){
+        print("========================generation: " + generation + "================================");
         var weighted_population = [];
         for(var i=0; i<population.length;i++){
             var fitness_val = fitness(population[i]);
-            if(fitness_val == 0){
+            if(fitness_val <= 20){
                 var pair =[population[i],1];
             }
             else{
@@ -161,27 +147,32 @@ function GA(SE){
             }    
             weighted_population.push(pair);
         }
+        var population_size = weighted_population.length
         var population =[];
-        for(var i=0; i<weighted_population.length/2;i++){
+        for(var i=0; i<population_size/2;i++){
             var solution1 = weighted_choice(weighted_population);
+            print("solution1 is: " + solution1 + " fitness value " + fitness(solution1));
             var solution2 = weighted_choice(weighted_population);
+            print("solution2 is: " + solution2 + " fitness value " + fitness(solution2));
             var sch1,ch2;
-            //var pos = Math.floor(Math.random() * (solution1.length-1));
-            var pos = 5;
-            var found = valid(solution1,solution2,pos);
+            var found = valid(solution1,solution2);
             if(found == false){
-                [sch1,sch2] = crossover(solution1, solution2,pos);
-                //population.push(solution1);
-                //population.push(solution2);
+                [sch1,sch2] = crossover(solution1, solution2);
+                print("sch1 is: " + sch1);
+                print("sch2 is: " + sch2);
+                population.push(solution1);
+                population.push(solution2);
                 population.push(mutate(sch1));
                 population.push(mutate(sch2));
             }
             else{
-                population.push(solution1);
+                print("********************no child***********************")
+                population.push((solution1));
                 population.push(solution2);
             }
 
         }
+  
         var fittest_solution = population[0];
         var minimum_fitness = fitness(population[0]);
         for(var i=0; i<population.length;i++){
@@ -192,13 +183,14 @@ function GA(SE){
             }
         }   
         print("Fittest value in Generation:" + generation + " is: " + minimum_fitness);
+        print("the best solution is: "+fittest_solution);
     }
     return 0;
     
 }   
 
 
-//GA([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20])
+GA([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20])
 
 
                   
